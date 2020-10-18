711b33a2244fd2258147f9190dbd96b74fa2a6d1
Vishal Tanwar <vishal.tanwar@outlook.com>
2020-10-18 23:30:16 +0530

    added ITRACKUserManager for creating users and superusers

---
+++ src/user/managers.py
@@ -0,0 +1,31 @@
+from django.contrib.auth.base_user import BaseUserManager
+from django.utils.translation import gettext_lazy as _
+
+
+class ITRACKUserManager(BaseUserManager):
+    """
+    iTrack User manager
+    """
+
+    def create_user(self, email, password, **extra_fields):
+
+        if not email:
+            raise ValueError(_("Email must be set"))
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_super_user(self, email, password, **extra_fields):
+
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_active", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError(_("Superuser must have is_staff=True."))
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError(_("Superuser must have is_superuser=True."))
+
+        return self.create_user(email, password, **extra_fields)
