6a43cc4b9b8e541c3b2d743c241db10173d8a8e2
Vishal Tanwar <vishal.tanwar@outlook.com>
2020-10-18 23:29:48 +0530

    added custom user model

--- src/user/models.py
+++ src/user/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, Group
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+from .managers import ITRACKUserManager
+
+
+class User(AbstractUser):
+    """
+    iTrack User model on top of django's built-in user model
+    """
+
+    username = None
+    groups = None
+    USERNAME_FIELD = "email"
+    objects = ITRACKUserManager()
+    REQUIRED_FIELDS = []
+    email = models.EmailField(_("email address"), unique=True)
+    is_active = models.BooleanField(
+        _("active"),
+        default=False,
+        help_text=_("Designates whether this user should be treated as active. "),
+    )
+    group = models.ForeignKey(
+        Group,
+        verbose_name=_("groups"),
+        blank=True,
+        help_text=_(
+            "The groups this user belongs to. A user will get all permissions "
+            "granted to each of their groups."
+        ),
+        related_name="user_set",
+        related_query_name="user",
+        on_delete=models.PROTECT,
+    )
+
+    class Meta(AbstractUser.Meta):
+        db_table = "user"
