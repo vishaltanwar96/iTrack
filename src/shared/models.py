547321e4e9749a9c7ae3b973d5c599b0cc485d77
Vishal Tanwar <vishal.tanwar@outlook.com>
2020-10-18 23:41:02 +0530

    added shared models

--- src/shared/models.py
+++ src/shared/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+class AbstractIdName(models.Model):
+    """
+    Abstract Model for id name Models
+    """
+
+    name = models.CharField(max_length=100)
+
+    class Meta:
+        abstract = True
+
+
+class AbstractRemarksHistory(models.Model):
+    remarks = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Status(AbstractIdName):
+    class Meta:
+        db_table = "status"
+
+
+class Criticality(AbstractIdName):
+    class Meta:
+        db_table = "criticality"
