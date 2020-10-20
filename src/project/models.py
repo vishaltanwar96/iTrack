fb72f0e8a206a40e99f1ced90bd7632aa785cce1
Vishal Tanwar <vishal.tanwar@outlook.com>
2020-10-20 21:07:39 +0530

    added models Project & ProjectRemarksHistory

--- src/project/models.py
+++ src/project/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+from user.models import User
+from shared.models import AbstractRemarksHistory, EntityMixin
+
+
+class Project(EntityMixin):
+    users = models.ManyToManyField(
+        User,
+        related_name="user_set",
+        related_query_name="user_set",
+    )
+
+    class Meta:
+        db_table = "project"
+
+
+class ProjectRemarksHistory(AbstractRemarksHistory):
+    project = models.ForeignKey(Project, on_delete=models.SET_NULL, null=True)
+
+    class Meta:
+        db_table = "project_remarks_history"
