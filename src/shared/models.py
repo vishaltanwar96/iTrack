7abdcf5250eab7a854d782df8607b0ec8562fc32
Vishal Tanwar <vishal.tanwar@outlook.com>
2020-10-20 21:07:54 +0530

    Added EntityMixin

--- src/shared/models.py
+++ src/shared/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.core import validators
+from user.models import User
 
 
 class AbstractIdName(models.Model):
@@ -15,6 +17,7 @@ class AbstractIdName(models.Model):
 class AbstractRemarksHistory(models.Model):
     remarks = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
 
 
 class Status(AbstractIdName):
@@ -25,3 +28,31 @@ class Status(AbstractIdName):
 class Criticality(AbstractIdName):
     class Meta:
         db_table = "criticality"
+
+
+class EntityMixin(models.Model):
+    name = models.CharField(
+        max_length=100,
+        validators=(
+            validators.MinLengthValidator(
+                limit_value=2,
+                message="Project name need to be atleast 2 characters long",
+            ),
+        ),
+    )
+    description = models.TextField(
+        validators=(
+            validators.MinLengthValidator(
+                limit_value=10,
+                message="Project description must be atleast 10 characters long",
+            ),
+        ),
+    )
+    criticality = models.ForeignKey(Criticality, on_delete=models.PROTECT)
+    status = models.ForeignKey(Status, on_delete=models.PROTECT)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    created_by = models.ForeignKey(User, on_delete=models.PROTECT)
+
+    class Meta:
+        abstract = True
