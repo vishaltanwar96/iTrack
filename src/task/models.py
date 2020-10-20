17ca0da7e82f111197d14e976dee12ffadfaf071
Vishal Tanwar <vishal.tanwar@outlook.com>
2020-10-20 21:08:18 +0530

    Added Task and TaskRemarksHistory models

--- src/task/models.py
+++ src/task/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+from user.models import User
+from project.models import Project
+from shared.models import EntityMixin, AbstractRemarksHistory
+
+
+class Task(EntityMixin):
+    assigned_to = models.ForeignKey(
+        User, on_delete=models.SET_NULL, related_name="responsible", null=True
+    )
+    assigned_by = models.ForeignKey(
+        User, on_delete=models.SET_NULL, related_name="creator", null=True
+    )
+    reviewed_by = models.ForeignKey(
+        User, on_delete=models.SET_NULL, related_name="reviewer", null=True
+    )
+    project = models.ForeignKey(Project, on_delete=models.SET_NULL, null=True)
+    expected_completion_date = models.DateField()
+    actual_completion_date = models.DateField(null=True)
+
+    class Meta:
+        db_table = "task"
+
+
+class TaskRemarksHistory(AbstractRemarksHistory):
+    task = models.ForeignKey(Task, on_delete=models.SET_NULL, null=True)
+
+    class Meta:
+        db_table = "task_remarks_history"
