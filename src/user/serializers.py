abd823acf1b9bebc55a58e8ee682fb8803140d59
Vishal Tanwar <vishal.tanwar@outlook.com>
2020-11-14 00:00:26 +0530

    Added a UserSerializer

---
+++ src/user/serializers.py
@@ -0,0 +1,44 @@
+from django.core import exceptions
+from django.contrib.auth import password_validation
+from rest_framework import serializers
+
+from .models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    """."""
+
+    confirm_password = serializers.CharField(write_only=True)
+
+    @staticmethod
+    def validate_password(password):
+        """."""
+
+        try:
+            password_validation.validate_password(password)
+        except exceptions.ValidationError as ve:
+            raise serializers.ValidationError(ve.messages)
+        return password
+
+    def validate(self, attrs):
+        """."""
+
+        super(UserSerializer, self).validate(attrs)
+        if attrs["password"] != attrs.pop("confirm_password"):
+            raise serializers.ValidationError(
+                {"password": ["password doesn't match confirm_password"]}
+            )
+        return attrs
+
+    class Meta:
+        """."""
+
+        model = User
+        fields = "__all__"
+        extra_kwargs = {
+            "password": {"write_only": True},
+            "group": {"required": True},
+            "first_name": {"required": True},
+            "last_name": {"required": True},
+            "user_permissions": {"write_only": True},
+        }
