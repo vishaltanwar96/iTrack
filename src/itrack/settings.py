3cdc930505bb0f47a7bf3a44688c8578d17b4183
Vishal Tanwar <vishal.tanwar@outlook.com>
2020-11-13 23:58:30 +0530

    Added REST_FRAMEWORK settings

--- src/itrack/settings.py
+++ src/itrack/settings.py
@@ -135,6 +135,12 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+REST_FRAMEWORK = {
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated",
+    ]
+}
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
