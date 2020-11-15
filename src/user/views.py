010bbf0fd7338c285b0a200b58c5b759c2019d21
Vishal <vishal.tanwar@outlook.com>
2020-11-15 22:15:19 +0530

    override perform_create to send email to the user registering for verification.

--- src/user/views.py
+++ src/user/views.py
@@ -11,3 +11,13 @@ class UserRegistrationView(CreateAPIView):
     queryset = User.objects.all()
     permission_classes = [AllowAny]
     serializer_class = UserSerializer
+
+    def perform_create(self, serializer):
+        """."""
+
+        user = super(UserRegistrationView, self).perform_create(serializer)
+        # THIS EMAIL MUST BE REPLACED WITH A CELERY TASK AND EMAIL MESSAGE WITH A HTML MESSAGE TEMPLATE
+        user.email_user(
+            "[ITRACK] ACCOUNT HAS BEEN CREATED",
+            f"Greetings! {user.first_name.title()} {user.last_name.title()}\n\n Your iTrack user account has been created, Please follow the link below to verfiy/confirm your account",
+        )
