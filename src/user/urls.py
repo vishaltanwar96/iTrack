22493e4ef8ac8a76100106b8e80c2215a1b7706c
Vishal <vishal.tanwar@outlook.com>
2020-11-16 20:11:47 +0530

    added url to confirm user account

--- src/user/urls.py
+++ src/user/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 
-from .views import UserRegistrationView
+from .views import UserRegistrationView, UserAccountConfirmationView
 
 
-urlpatterns = [path("", UserRegistrationView.as_view())]
+urlpatterns = [
+    path("", UserRegistrationView.as_view()),
+    path("confirm/<str:signed_user_token>/", UserAccountConfirmationView.as_view()),
+]
