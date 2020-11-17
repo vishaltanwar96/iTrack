151e59e67a8c83a7d88fdfda644119250fb56dac
Vishal <vishal.tanwar@outlook.com>
2020-11-17 22:15:36 +0530

    added login and login refresh view

--- src/user/urls.py
+++ src/user/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-from .views import UserRegistrationView, UserAccountConfirmationView
+from .views import RegistrationView, AccountConfirmationView
 
 
 urlpatterns = [
-    path("", UserRegistrationView.as_view()),
-    path("confirm/<str:signed_user_token>/", UserAccountConfirmationView.as_view()),
+    path("", RegistrationView.as_view(), name="register"),
+    path(
+        "confirm/<str:signed_user_token>/",
+        AccountConfirmationView.as_view(),
+        name="account_confirmation",
+    ),
+    path("login/", TokenObtainPairView.as_view(), name="login"),
+    path("login/refresh/", TokenRefreshView.as_view(), name="login_refresh"),
 ]
