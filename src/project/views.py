0a5bb2f6294bcbe845622f66a06d66d8c93df82d
Vishal <vishal.tanwar@outlook.com>
2020-11-23 22:12:17 +0530

    added ProjectViewSet

--- src/project/views.py
+++ src/project/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .models import Project
+from .serializers import ProjectSerializer
+from itrack.permissions import IsAccessAllowedToGroup
+
+
+class ProjectViewSet(viewsets.ModelViewSet):
+    """."""
+
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
+    permission_classes = [IsAccessAllowedToGroup]
