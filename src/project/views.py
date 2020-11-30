31056366938a2a6fcedeb0dca5e16815f5ad8167
Vishal <vishal.tanwar@outlook.com>
2020-11-30 21:24:36 +0530

    overridden create to associate the user creating the project

--- src/project/views.py
+++ src/project/views.py
@@ -1,4 +1,5 @@
-from rest_framework import viewsets
+from rest_framework import status, viewsets
+from rest_framework.response import Response
 
 from .models import Project
 from .serializers import ProjectSerializer
@@ -11,3 +12,21 @@ class ProjectViewSet(viewsets.ModelViewSet):
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
     permission_classes = [IsAccessAllowedToGroup]
+
+    def perform_create(self, serializer):
+        """."""
+
+        serializer.save(created_by=self.request.user)
+
+    def create(self, request, *args, **kwargs):
+        """."""
+
+        serializer = self.get_serializer(
+            data={**request.data, "users": [request.user.id]}
+        )
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(
+            serializer.data, status=status.HTTP_201_CREATED, headers=headers
+        )
