1dcdec914dc8afb012f7f67629b9555d9ffd6dbe
Vishal <vishal.tanwar@outlook.com>
2020-12-03 21:51:22 +0530

    updated login_urlpatterns and password_reset_urlpatterns

--- src/user/urls.py
+++ src/user/urls.py
@@ -1,8 +1,33 @@
-from django.urls import path
+from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-from .views import RegistrationView, AccountConfirmationView
+from .views import (
+    RegistrationView,
+    AccountConfirmationView,
+    PasswordResetEmailView,
+    PasswordResetConfirmationView,
+    PasswordResetChangeView,
+)
 
+
+login_urlpatterns = [
+    path("", TokenObtainPairView.as_view(), name="login"),
+    path("refresh/", TokenRefreshView.as_view(), name="login_refresh"),
+]
+
+password_reset_urlpatterns = [
+    path("", PasswordResetEmailView.as_view(), name="password_reset_email"),
+    path(
+        "confirm/<str:signed_user_token>/",
+        PasswordResetConfirmationView.as_view(),
+        name="password_reset_confirm",
+    ),
+    path(
+        "change/<str:signed_user_token>/",
+        PasswordResetChangeView.as_view(),
+        name="password_reset_change",
+    ),
+]
 
 urlpatterns = [
     path("", RegistrationView.as_view(), name="register"),
@@ -11,6 +36,6 @@ urlpatterns = [
         AccountConfirmationView.as_view(),
         name="account_confirmation",
     ),
-    path("login/", TokenObtainPairView.as_view(), name="login"),
-    path("login/refresh/", TokenRefreshView.as_view(), name="login_refresh"),
+    path("login/", include(login_urlpatterns)),
+    path("password-reset/", include(password_reset_urlpatterns)),
 ]
