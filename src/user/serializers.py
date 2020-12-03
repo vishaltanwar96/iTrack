0b8c06f4922d00cf334d22feeae07ae5ef36a267
Vishal <vishal.tanwar@outlook.com>
2020-12-03 21:50:38 +0530

    added PasswordResetEmailSerializer and PasswordResetChangeSerializer

--- src/user/serializers.py
+++ src/user/serializers.py
@@ -42,3 +42,39 @@ class UserSerializer(serializers.ModelSerializer):
             "last_name": {"required": True},
             "user_permissions": {"write_only": True},
         }
+
+
+class PasswordResetEmailSerializer(serializers.Serializer):
+    """."""
+
+    email = serializers.EmailField(required=True)
+
+
+class PasswordResetChangeSerializer(serializers.Serializer):
+    """."""
+
+    new_password = serializers.CharField(required=True, write_only=True)
+    confirm_new_password = serializers.CharField(required=True, write_only=True)
+
+    # Find a way such that methods below can be inherited in UserSerializer since the signature is almost same
+    # Only varible name difference is there
+    # (Can be achieved if the new_password -> password and confirm_new_password -> confirm_password)
+    @staticmethod
+    def validate_new_password(new_password):
+        """."""
+
+        try:
+            password_validation.validate_password(new_password)
+        except exceptions.ValidationError as ve:
+            raise serializers.ValidationError(ve.messages)
+        return new_password
+
+    def validate(self, attrs):
+        """."""
+
+        super(PasswordResetChangeSerializer, self).validate(attrs)
+        if attrs["new_password"] != attrs.pop("confirm_new_password"):
+            raise serializers.ValidationError(
+                {"new_password": ["new_password doesn't match confirm_new_password"]}
+            )
+        return attrs
