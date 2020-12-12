e06b3a85ba6cd8fa61e45316cf76dba9f0b79ed6
Vishal <vishal.tanwar@outlook.com>
2020-12-12 18:45:01 +0530

    added TodoSerializer

---
+++ src/todos/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+
+from .models import Todo
+
+
+class TodoSerializer(serializers.ModelSerializer):
+    """."""
+
+    class Meta:
+
+        model = Todo
+        fields = "__all__"
+        read_only_fields = ("belongs_to",)
+        extra_kwargs = {
+            "belongs_to": {"default": serializers.CurrentUserDefault()},
+        }
