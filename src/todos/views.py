ee35a314f7296b2fcffba524af9af7ccba53602e
Vishal <vishal.tanwar@outlook.com>
2020-12-12 18:46:34 +0530

    added TodoViewSet

--- src/todos/views.py
+++ src/todos/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.db.models import QuerySet
+from rest_framework import viewsets, permissions
 
-# Create your views here.
+from .models import Todo
+from .serializers import TodoSerializer
+
+
+class TodoViewSet(viewsets.ModelViewSet):
+    """."""
+
+    permission_classes = [permissions.DjangoObjectPermissions]
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
+
+    def perform_create(self, serializer):
+        """."""
+
+        serializer.save(belongs_to=self.request.user)
+
+    def get_queryset(self):
+        """."""
+
+        assert self.queryset is not None, (
+            "'%s' should either include a `queryset` attribute, "
+            "or override the `get_queryset()` method." % self.__class__.__name__
+        )
+
+        queryset = self.queryset
+        queryset = queryset.filter(belongs_to=self.request.user)
+        if isinstance(queryset, QuerySet):
+            # Ensure queryset is re-evaluated on each request.
+            queryset = queryset.all()
+        return queryset
