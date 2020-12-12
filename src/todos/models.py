0e3e7179e077c526aafeadd8e2538ee6152989ec
Vishal <vishal.tanwar@outlook.com>
2020-12-12 18:46:11 +0530

    added description and is complete boolean to todos model

--- src/todos/models.py
+++ src/todos/models.py
@@ -6,9 +6,11 @@ from user.models import User
 class Todo(models.Model):
 
     todo = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     belongs_to = models.ForeignKey(User, on_delete=models.CASCADE)
+    is_complete = models.BooleanField(default=False)
 
     class Meta:
         db_table = "todo"
