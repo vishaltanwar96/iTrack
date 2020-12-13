5f9bd6a61d827bf50770d2e9cb4d87cdd3952696
Vishal <vishal.tanwar@outlook.com>
2020-12-13 14:06:42 +0530

    added Criticality, Group and Status List Views

--- src/shared/views.py
+++ src/shared/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from django.contrib.auth.models import Group
 
-# Create your views here.
+from .models import Status, Criticality
+from .serializers import StatusSerializer, CriticalitySerializer, GroupSerializer
+
+
+class StatusView(generics.ListAPIView):
+    """."""
+
+    permission_classes = [permissions.IsAuthenticated]
+    queryset = Status.objects.all()
+    serializer_class = StatusSerializer
+
+
+class GroupView(generics.ListAPIView):
+    """."""
+
+    permission_classes = [permissions.IsAuthenticated]
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+
+
+class CriticalityView(generics.ListAPIView):
+    """."""
+
+    permission_classes = [permissions.IsAuthenticated]
+    queryset = Criticality.objects.all()
+    serializer_class = CriticalitySerializer
