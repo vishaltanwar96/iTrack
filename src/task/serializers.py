bb6ef3ee9817b1c3565fd9bf85fafc1e51a50a49
Vishal <vishal.tanwar@outlook.com>
2020-12-20 22:29:33 +0530

    added TaskSerializer

---
+++ src/task/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+
+from .models import Task
+
+
+class TaskSerializer(serializers.ModelSerializer):
+    """."""
+
+    class Meta:
+        model = Task
+        fields = "__all__"
+        read_only_fields = (
+            "created_at",
+            "updated_at",
+            "actual_completion_date",
+            "reviewed_by",
+        )
