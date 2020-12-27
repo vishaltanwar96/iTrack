e5086447ad3a0040505dfbd2e08a4f8678b153e3
Vishal <vishal.tanwar@outlook.com>
2020-12-27 21:39:40 +0530

    added a past_date_validator that check if the date is lt now.

---
+++ src/itrack/model_field_validation.py
@@ -0,0 +1,10 @@
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
+from django.utils import timezone
+
+
+def past_date_validator(value):
+    """Checks if the date is less than current date"""
+
+    if value < timezone.now().date():
+        raise ValidationError(_("cannot be less than current date"))
