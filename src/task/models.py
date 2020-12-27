e861f66bb7008c11f4b3b7dce32faa66dea62f8f
Vishal <vishal.tanwar@outlook.com>
2020-12-27 21:39:59 +0530

    added past_date_validation in model fields

--- src/task/models.py
+++ src/task/models.py
@@ -3,6 +3,7 @@ from django.db import models
 from user.models import User
 from project.models import Project
 from shared.models import AbstractEntity, AbstractRemarksHistory
+from itrack.model_field_validation import past_date_validator
 
 
 class Task(AbstractEntity):
@@ -14,13 +15,15 @@ class Task(AbstractEntity):
         User, on_delete=models.CASCADE, related_name="creator"
     )
     reviewed_by = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="reviewer"
+        User, on_delete=models.CASCADE, related_name="reviewer", null=True
     )
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
     # dependency limited to one person for now
     dependency = models.CharField(max_length=255, null=True)
-    expected_completion_date = models.DateField()
-    actual_completion_date = models.DateField(null=True)
+    expected_completion_date = models.DateField(validators=[past_date_validator])
+    actual_completion_date = models.DateField(
+        null=True, validators=[past_date_validator]
+    )
 
     class Meta:
         db_table = "task"
