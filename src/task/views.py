854cc373f426c40a5426d3b149325342544e4f94
Vishal <vishal.tanwar@outlook.com>
2021-01-10 00:05:26 +0530

    added list method to list user tasks and get_queryset method to only show tasks of the projects in which the user is associated

--- src/task/views.py
+++ src/task/views.py
@@ -15,12 +15,28 @@ class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
     permission_classes = [IsAccessAllowedToGroup]
-    NEW_TASK_EVENT = "NEW_TASK"
 
     def perform_create(self, serializer):
         """."""
 
-        return serializer.save(created_by=self.request.user)
+        return serializer.save(
+            created_by=self.request.user, assigned_by=self.request.user
+        )
+
+    def list(self, request, *args, **kwargs):
+        """."""
+
+        queryset = self.filter_queryset(self.get_queryset())
+
+        queryset = queryset.filter(assigned_to=request.user)
+
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
 
     def create(self, request, *args, **kwargs):
         """."""
@@ -29,7 +45,6 @@ class TaskViewSet(viewsets.ModelViewSet):
             data={
                 **request.data,
                 "status": Status.objects.get(name=constants.ASSIGNED).id,
-                "assigned_by": request.user.id,
             }
         )
         serializer.is_valid(raise_exception=True)
@@ -42,8 +57,8 @@ class TaskViewSet(viewsets.ModelViewSet):
         assigned_to_user = serializer.validated_data["assigned_to"]
         task = self.perform_create(serializer)
         assigned_to_user.email_user(
-            subject=EMAIL_SUBJECT[self.NEW_TASK_EVENT],
-            message=EMAIL_BODY[self.NEW_TASK_EVENT].format(
+            subject=EMAIL_SUBJECT[constants.NEW_TASK],
+            message=EMAIL_BODY[constants.NEW_TASK].format(
                 assignee_first_name=assigned_to_user.first_name,
                 assignee_last_name=assigned_to_user.last_name,
                 assignor_first_name=task.assigned_by.first_name,
@@ -56,3 +71,19 @@ class TaskViewSet(viewsets.ModelViewSet):
         return Response(
             serializer.data, status=status.HTTP_201_CREATED, headers=headers
         )
+
+    def get_queryset(self):
+        """."""
+
+        return self.queryset.filter(
+            project__in=[project for project in self.request.user.projects.all()]
+        )
+
+    def destroy(self, request, *args, **kwargs):
+        """."""
+
+        instance = self.get_object()
+        instance.is_active = False
+        instance.save()
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
