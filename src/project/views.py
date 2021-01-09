2cab16e1f54c7965e64d3bd6a5a304f1168974ba
Vishal <vishal.tanwar@outlook.com>
2021-01-09 19:30:19 +0530

    removed create since it's not needed anymore to be overridden

--- src/project/views.py
+++ src/project/views.py
@@ -59,20 +59,7 @@ class ProjectViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         """."""
 
-        serializer.save(created_by=self.request.user)
-
-    def create(self, request, *args, **kwargs):
-        """."""
-
-        serializer = self.get_serializer(
-            data={**request.data, "users": [request.user.id]}
-        )
-        serializer.is_valid(raise_exception=True)
-        self.perform_create(serializer)
-        headers = self.get_success_headers(serializer.data)
-        return Response(
-            serializer.data, status=status.HTTP_201_CREATED, headers=headers
-        )
+        serializer.save(created_by=self.request.user, users=[self.request.user.id])
 
     @action(
         methods=["get"],
