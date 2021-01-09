69a5c9832aa20b2d772241a8cf39c9991fe574e8
Vishal <vishal.tanwar@outlook.com>
2021-01-09 20:21:23 +0530

    updated communication_messages

--- src/itrack/communication_messages.py
+++ src/itrack/communication_messages.py
@@ -1,5 +1,5 @@
 ITRACK_LABEL = "[iTrack]"
-ITRACK_SIGNATURE = "Sincerely\niTrack"
+ITRACK_SIGNATURE = "Sincerely,\niTrack Notification Manager"
 
 EMAIL_SUBJECT = {
     "NEW_TASK": "%s New Task Alert!" % ITRACK_LABEL,
@@ -12,34 +12,34 @@ EMAIL_SUBJECT = {
 
 EMAIL_BODY = {
     "NEW_TASK": (
-        "Dear {assignee_first_name} {assignee_last_name},\n"
+        "Dear {assignee_first_name} {assignee_last_name},\n\n"
         "A new task has been assigned to you "
         "by {assignor_first_name} {assignor_last_name}\n\nDetails:\n"
         "Task ID: #{task_id}\nTask: {task_name}\n\n%s" % ITRACK_SIGNATURE
     ),
     "NEW_PROJECT_ASSOCIATION": (
-        "Dear {user_first_name} {user_last_name},\n"
+        "Dear {user_first_name} {user_last_name},\n\n"
         "Welcome to the Project-{project_name}, We look forward for your contribution\n\n"
         "%s" % ITRACK_SIGNATURE
     ),
     "USER_ACCOUNT_CREATED": (
-        "Dear {user_first_name} {user_last_name},\n"
+        "Dear {user_first_name} {user_last_name},\n\n"
         "Your iTrack user account has been created, Please use the following code to activate your account.\n\n"
         "Code: {secret_key_signed_code}\n\n"
         "Note: The code expires in 2 days from the time received.\n\n%s"
         % ITRACK_SIGNATURE
     ),
     "RESEND_ACTIVATION_CODE": (
-        "Dear {user_first_name} {user_last_name},\n"
+        "Dear {user_first_name} {user_last_name},\n\n"
         "Please use the following code to activate your account.\n\n"
         "Code: {secret_key_signed_code}\n\n%s" % ITRACK_SIGNATURE
     ),
     "ACCOUNT_CONFIRMATION": (
-        "Dear {user_first_name} {user_last_name},\n"
+        "Dear {user_first_name} {user_last_name},\n\n"
         "Your iTrack account has been successfully confirmed\n\n%s" % ITRACK_SIGNATURE
     ),
     "RESET_PASSWORD": (
-        "Dear {user_first_name} {user_last_name},\n"
+        "Dear {user_first_name} {user_last_name},\n\n"
         "Please enter the code below to reset your password\n\n"
         "Code: {secret_key_signed_code}\n\n"
         "Note: The code expires in 2 days from the time received.\n\n%s"
