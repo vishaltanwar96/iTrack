c21f2281d075ec05befb5d3bb65bbd38b4299096
Vishal <vishal.tanwar@outlook.com>
2021-01-09 18:04:14 +0530

    Updated EMAIL_MESSAGES

--- src/itrack/communication_messages.py
+++ src/itrack/communication_messages.py
@@ -1,10 +1,13 @@
+ITRACK_LABEL = "[iTrack]"
+ITRACK_SIGNATURE = "Sincerely\niTrack"
+
 EMAIL_SUBJECT = {
-    "NEW_TASK": "[iTrack] New Task Alert!",
-    "NEW_PROJECT_ASSOCIATION": "[iTrack] Welcome to the project!",
-    "USER_ACCOUNT_CREATED": "[iTrack] Welcome to iTrack!",
-    "RESEND_ACTIVATION_CODE": "[iTrack] Account Activation Code",
-    "ACCOUNT_CONFIRMATION": "[iTrack] Account Confirmed!",
-    "RESET_PASSWORD": "[iTrack] Password Assistance",
+    "NEW_TASK": "%s New Task Alert!" % ITRACK_LABEL,
+    "NEW_PROJECT_ASSOCIATION": "%s Welcome to the project!" % ITRACK_LABEL,
+    "USER_ACCOUNT_CREATED": "%s Welcome to iTrack!" % ITRACK_LABEL,
+    "RESEND_ACTIVATION_CODE": "%s Account Activation Code" % ITRACK_LABEL,
+    "ACCOUNT_CONFIRMATION": "%s Account Confirmed!" % ITRACK_LABEL,
+    "RESET_PASSWORD": "%s Password Assistance" % ITRACK_LABEL,
 }
 
 EMAIL_BODY = {
@@ -12,36 +15,34 @@ EMAIL_BODY = {
         "Dear {assignee_first_name} {assignee_last_name},\n"
         "A new task has been assigned to you "
         "by {assignor_first_name} {assignor_last_name}\n\nDetails:\n"
-        "Task ID: #{task_id}\nTask: {task_name}\n\nSincerely\niTrack"
+        "Task ID: #{task_id}\nTask: {task_name}\n\n%s" % ITRACK_SIGNATURE
     ),
     "NEW_PROJECT_ASSOCIATION": (
         "Dear {user_first_name} {user_last_name},\n"
         "Welcome to the Project-{project_name}, We look forward for your contribution\n\n"
-        "Sincerely\niTrack"
+        "%s" % ITRACK_SIGNATURE
     ),
     "USER_ACCOUNT_CREATED": (
         "Dear {user_first_name} {user_last_name},\n"
         "Your iTrack user account has been created, Please use the following code to activate your account.\n\n"
         "Code: {secret_key_signed_code}\n\n"
-        "Note: The code expires in 2 days from the time received.\n\n"
-        "Sincerely\niTrack"
+        "Note: The code expires in 2 days from the time received.\n\n%s"
+        % ITRACK_SIGNATURE
     ),
     "RESEND_ACTIVATION_CODE": (
         "Dear {user_first_name} {user_last_name},\n"
         "Please use the following code to activate your account.\n\n"
-        "Code: {secret_key_signed_code}\n\n"
-        "Sincerely\niTrack"
+        "Code: {secret_key_signed_code}\n\n%s" % ITRACK_SIGNATURE
     ),
     "ACCOUNT_CONFIRMATION": (
         "Dear {user_first_name} {user_last_name},\n"
-        "Your iTrack account has been successfully confirmed\n\n"
-        "Sincerely\niTrack"
+        "Your iTrack account has been successfully confirmed\n\n%s" % ITRACK_SIGNATURE
     ),
     "RESET_PASSWORD": (
         "Dear {user_first_name} {user_last_name},\n"
         "Please enter the code below to reset your password\n\n"
-        "Code: {signing.dumps(user.id)}\n\n"
-        "Note: The code expires in 2 days from the time received.\n\n"
-        "Sincerely\niTrack"
+        "Code: {secret_key_signed_code}\n\n"
+        "Note: The code expires in 2 days from the time received.\n\n%s"
+        % ITRACK_SIGNATURE
     ),
 }
