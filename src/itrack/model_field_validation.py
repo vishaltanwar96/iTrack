bb6dcc07a34ad477f92b4902b77ee7e424fecf38
Vishal <vishal.tanwar@outlook.com>
2021-01-09 20:41:17 +0530

    updated as timezone since database stores everything in UTC timezone

--- src/itrack/model_field_validation.py
+++ src/itrack/model_field_validation.py
@@ -6,5 +6,5 @@ from django.utils import timezone
 def past_date_validator(value):
     """Checks if the date is less than current date"""
 
-    if value < timezone.now().date():
+    if value < timezone.now().astimezone().date():
         raise ValidationError(_("cannot be less than current date"))
